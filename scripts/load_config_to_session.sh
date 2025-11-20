45ab892ded6a0220e26c52c0572906beca77ca87
aicreativeexplorer <aicreativeexplorer@gmail.com>
2025-11-20 14:46:47 +0530

    Update load_config_to_session.sh

--- scripts/load_config_to_session.sh
+++ scripts/load_config_to_session.sh
@@ -1,1 +1,5 @@
+#!/usr/bin/env bash
+# copy notebook_config into /content for runtime use
+cp /content/YT-Automation/notebook_config.txt /content/notebook_config.txt || true
+echo "config copied to /content/notebook_config.txt"
 
