e2876663dfbb9457815101cc75dfa9816d83bc17
aicreativeexplorer <aicreativeexplorer@gmail.com>
2025-11-20 14:46:17 +0530

    Update clone_generative_models.sh

--- scripts/clone_generative_models.sh
+++ scripts/clone_generative_models.sh
@@ -1,1 +1,7 @@
+#!/usr/bin/env bash
+set -e
+if [ ! -d /content/generative-models ]; then
+  git clone https://github.com/Stability-AI/generative-models.git /content/generative-models
+fi
+pip install -q -e /content/generative-models || true
 
