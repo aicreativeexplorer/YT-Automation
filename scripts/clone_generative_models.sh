1d544a686d54a3e6fb15d2ee7e0ca96096978609
aicreativeexplorer <aicreativeexplorer@gmail.com>
2025-12-05 15:54:39 +0530

    Delete scripts directory

--- scripts/clone_generative_models.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-if [ ! -d /content/generative-models ]; then
-  git clone https://github.com/Stability-AI/generative-models.git /content/generative-models
-fi
-pip install -q -e /content/generative-models || true
-
--- scripts/load_config_to_session.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# copy notebook_config into /content for runtime use
-cp /content/YT-Automation/notebook_config.txt /content/notebook_config.txt || true
-echo "config copied to /content/notebook_config.txt"
-
